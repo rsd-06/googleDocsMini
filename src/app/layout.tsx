84f485574392637a5d59635c72dd837b92034c04
rsd-06 <rsudharshan2006@gmail.com>
2025-11-30 11:48:25 +0530

    Setup of Convex Database and Server Done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 
 import './globals.css';
 
@@ -25,7 +26,9 @@ export default function RootLayout({
         className={inter.className}
       >
         <NuqsAdapter>
-          {children}
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
         </NuqsAdapter>
       </body>
     </html>
